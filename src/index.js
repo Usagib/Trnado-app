ffa0c79d52571a3e4ad635d9332e39baf79a2934
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-06-03 15:16:10 -0500

    fetching data from api and display finsihed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,93 @@ import header from './view/header.js';
 import content from './view/content.js';
 
 const main = document.getElementById('main-container');
-main.innerHTML = header;
 const weatherContainer = document.createElement('div');
 weatherContainer.setAttribute('class', 'container');
+main.innerHTML = header;
 weatherContainer.innerHTML = content;
 main.appendChild(weatherContainer);
 
+
+const form = document.getElementById('weatherForm');
+const cityInput = document.getElementById('city');
 const apiKey = '48edfbeefc2786e75b112e9fc0536b00';
+const degrees = document.getElementsByName('degree');
+let unit = 'metric';
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  degrees.forEach((metric) => {
+    if(metric.checked) unit = metric.value;
+  });
+
+  const cityName = cityInput.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
+  console.log(url);
+  fetch(url, {mode:'cors'}).then(response => response.json())
+  .then( data => {
+    const {
+      coord, weather, main, wind, sys, name
+    } = data;
+
+    const showCity = document.getElementById('showCity');
+    const showTemperature = document.getElementById('showTemp');
+    const showWeather = document.getElementById('showWeather');
+    const showMinMax = document.getElementById('showMinMax');
+    const showWind = document.getElementById('showWind');
+    const showHumidity = document.getElementById('showHumid');
+    const background = document.getElementsByTagName('body')
+    let symbol = null;
+    if (unit === 'metric'){
+      symbol = 'C°';
+    } else {
+      symbol = 'F°';
+    }
+
+    showCity.innerText = `${name}, ${sys.country}`;
+    showTemperature.innerText = `${main.temp} ${symbol} : Feels like: ${main.feels_like}`;
+    showWeather.innerText = `${weather[0]['main']}, ${weather[0]['description']}`;
+    showMinMax.innerText = `Max: ${main.temp_max}, Min: ${main.temp_min}`;
+    showWind.innerText = `Wind speed: ${wind.speed} mph`;
+    showHumidity.innerText = `Humidity: ${main.humidity}`;
+
+    if(weather[0]['description'] === 'clear sky') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1506588345361-5e12b7840845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'few clouds') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1474218861938-d6b14818c8e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'scattered clouds') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1543226862-39202f29696f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'broken clouds') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1581788945370-3fe92ade72db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'shower rain') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1519692933481-e162a57d6721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'rain') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1428592953211-077101b2021b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'thunderstorm') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1472145246862-b24cf25c4a36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'snow') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1465220183275-1faa863377e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+    if(weather[0]['description'] === 'mist') {
+      document.body.style.background = "#ffffff url('https://images.unsplash.com/photo-1438803235109-d737bc3129ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80') no-repeat center center fixed";
+      document.body.style.backgroundSize = "cover";
+    }
+  });
+
+});
--- src/view/content.js
+++ src/view/content.js
@@ -5,25 +5,38 @@ const content =
       <div class="text-center">
         <img src="../src/view/img/main.png" class="img-fluid main-image" alt="">
         <h1 class="font-weight-light text-center">Trnado, How's the weather in?</h1>
-        <form class="" action="index.html" method="post">
+        <form id="weatherForm">
           <div class="input-group">
             <input type="text" class="form-control" id="city" placeholder="Search weather by city">
               <div class="input-group-append">
-                <span class="input-group-text" id="search-button">Go!</span>
+                <input type="submit" class="input-group-text" id="go-button" value="Go!">
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                  <label class="btn btn-secondary">
+                    <input type="radio" name="degree" id="celsius" autocomplete="off" value="metric" checked> C°
+                  </label>
+                  <label class="btn btn-secondary">
+                    <input type="radio" name="degree" id="farenheight" value="imperial" autocomplete="off"> F°
+                  </label>
+
+</div>
               </div>
           </div>
         </form>
       </div>
 
-      <div class="card mb-3">
-        <!-- <img class="card-img-top" style="height: 70px;" src="../src/view/img/main.png" alt="city image by api">
--->
+      <div class="card">
         <div class="card-body">
-          <h5 class="card-title">City</h5>
-          <p class="card-text">38°F</p>
-          <p class="card-text">Sunny.</p>
+          <h4 class="card-title" id="showCity"></h4>
+          <h3 class="card-text" id="showTemp"></h3>
+          <p class="card-text" id="showMinMax"></p>
+          <p class="card-text" id="showWeather"></p>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item" id="showWind"></li>
+            <li class="list-group-item" id="showHumid"></li>
+          </ul>
         </div>
       </div>
+
 
     </div>
   </div>
